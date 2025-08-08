9bf2a3f2ed225ae866fb2ac167e9cb1f44b9baee
liangwang <liangwang@iflytek.com>
2025-08-08 13:57:41 +0800

    参考https://github.com/BurntSushi/xgbutil/blob/master/_examples/simple-keybinding/main.go, 确认全局快捷键绑定是可行的

--- test/main.go
+++ test/main.go
@@ -1,61 +1,78 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os/exec"
-	"syscall"
 
-	"github.com/BurntSushi/xgb/xproto"
 	"github.com/BurntSushi/xgbutil"
+	"github.com/BurntSushi/xgbutil/keybind"
+	"github.com/BurntSushi/xgbutil/xevent"
 )
 
 func main() {
-	// 初始化 X11 连接
-	x, err := xgbutil.NewConn()
+	// Connect to the X server using the DISPLAY environment variable.
+	X, err := xgbutil.NewConn()
 	if err != nil {
-		log.Fatal("Failed to connect to X server:", err)
-	}
-	defer x.Conn().Close()
-
-	// 获取屏幕数量
-	screenCount := x.NumScreens()
-
-	// 绑定全局快捷键 Ctrl+Shift+Z
-	for i := 0; i < screenCount; i++ {
-		screen := x.Screen(i)
-		rootWindow := screen.Root
-
-		// 获取按键代码
-		keysym := xproto.KeysymZ
-		keycode, err := xproto.KeysymToKeycode(x.Conn(), keysym)
-		if err != nil {
-			log.Fatal("Failed to get keycode for keysym:", err)
-		}
-
-		// 设置修饰符
-		modifiers := xproto.ModifierControl | xproto.ModifierShift
-
-		// 绑定快捷键
-		xproto.GrabKey(x.Conn(), false, rootWindow, modifiers, keycode, xproto.GrabModeAsync, xproto.GrabModeAsync)
+		log.Fatal(err)
 	}
 
-	// 事件循环
-	for {
-		ev, err := x.Conn().NextEvent()
-		if err != nil {
-			log.Fatal("Failed to get event:", err)
-		}
+	// Anytime the keybind (mousebind) package is used, keybind.Initialize
+	// *should* be called once. It isn't strictly necessary, but allows your
+	// keybindings to persist even if the keyboard mapping is changed during
+	// run-time. (Assuming you're using the xevent package's event loop.)
+	keybind.Initialize(X)
 
-		switch ev := ev.(type) {
-		case xproto.KeyPressEvent:
-			fmt.Println("Key pressed:", ev.Detail)
-			// 执行通知命令
-			cmd := exec.Command("notify-send", "Hello")
-			cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-			if err := cmd.Start(); err != nil {
-				log.Fatal("Failed to start command:", err)
-			}
-		}
+	// Before attaching callbacks, wrap them in a callback function type.
+	// The keybind package exposes two such callback types: keybind.KeyPressFun
+	// and keybind.KeyReleaseFun.
+	cb1 := keybind.KeyPressFun(
+		func(X *xgbutil.XUtil, e xevent.KeyPressEvent) {
+			log.Println("Key press!")
+		})
+
+	// We can now attach the callback to a particular window and key
+	// combination. This particular example grabs a key on the root window,
+	// which makes it a global keybinding.
+	// Also, "Mod4-j" typically corresponds to pressing down the "Super" or
+	// "Windows" key on your keyboard, and then pressing the letter "j".
+	// N.B. This approach works by issuing a passive grab on the window
+	// specified. To respond to Key{Press,Release} events without a grab, use
+	// the xevent.Key{Press,Release}Fun callback function types instead.
+	err = cb1.Connect(X, X.RootWin(), "Mod4-j", true)
+
+	// A keybinding can fail if the key string could not be parsed, or if you're
+	// trying to bind a key that has already been grabbed by another client.
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	// We can even attach multiple callbacks to the same key.
+	err = keybind.KeyPressFun(
+		func(X *xgbutil.XUtil, e xevent.KeyPressEvent) {
+			log.Println("A second handler always happens after the first.")
+		}).Connect(X, X.RootWin(), "Mod4-j", true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Finally, if we want this client to stop responding to key events, we
+	// can attach another handler that, when run, detaches all previous
+	// handlers.
+	// This time, we'll show an example of a KeyRelease binding.
+	err = keybind.KeyReleaseFun(
+		func(X *xgbutil.XUtil, e xevent.KeyReleaseEvent) {
+			// Use keybind.Detach to detach the root window
+			// from all KeyPress *and* KeyRelease handlers.
+			keybind.Detach(X, X.RootWin())
+
+			log.Printf("Detached all Key{Press,Release}Events from the "+
+				"root window (%d).", X.RootWin())
+		}).Connect(X, X.RootWin(), "Mod4-Shift-q", true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Finally, start the main event loop. This will route any appropriate
+	// KeyPressEvents to your callback function.
+	log.Println("Program initialized. Start pressing keys!")
+	xevent.Main(X)
 }
