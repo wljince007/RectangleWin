a53c70e1223024d6d51c6d4a7fd8c6d46eaf1e10
liangwang <liangwang@iflytek.com>
2025-08-11 11:45:40 +0800

    窗口到下一个屏幕

--- test_winmgr/main.go
+++ test_winmgr/main.go
@@ -7,8 +7,52 @@ import (
 	"github.com/BurntSushi/xgbutil/ewmh"
 	"github.com/BurntSushi/xgbutil/keybind"
 	"github.com/BurntSushi/xgbutil/xevent"
+	"github.com/BurntSushi/xgbutil/xinerama"
 	"github.com/BurntSushi/xgbutil/xwindow"
 )
+
+func moveToNextMonitor(X *xgbutil.XUtil) {
+	// 获取所有显示器信息
+	screens, err := xinerama.PhysicalHeads(X)
+	if err != nil {
+		log.Fatal("获取显示器失败:", err)
+	}
+
+	// 获取活动窗口
+	win, err := ewmh.ActiveWindowGet(X)
+	if err != nil {
+		log.Fatal("获取活动窗口失败:", err)
+	}
+
+	// 获取窗口当前位置
+	winGeom, err := xwindow.New(X, win).Geometry()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 查找当前显示器索引
+	currentIdx := 0
+	for i, screen := range screens {
+		if winGeom.X() >= screen.X() && winGeom.X() < screen.X()+screen.Width() {
+			currentIdx = i
+			break
+		}
+	}
+
+	// 计算下一个显示器索引
+	nextIdx := (currentIdx + 1) % len(screens)
+	nextScreen := screens[nextIdx]
+
+	// 移动窗口到下一个显示器
+	err = ewmh.MoveresizeWindow(X, win,
+		nextScreen.X()+(winGeom.X()%screens[currentIdx].Width()),
+		nextScreen.Y()+(winGeom.Y()%screens[currentIdx].Height()),
+		winGeom.Width(),
+		winGeom.Height())
+	if err != nil {
+		log.Fatal("移动窗口失败:", err)
+	}
+}
 
 func main() {
 	// Connect to the X server
@@ -77,6 +121,13 @@ func main() {
 			}
 		},
 	).Connect(X, X.RootWin(), keystr, true) // true 表示自动抓取按键
+
+	// 绑定快捷键
+	keybind.KeyPressFun(
+		func(X *xgbutil.XUtil, e xevent.KeyPressEvent) {
+			moveToNextMonitor(X)
+		},
+	).Connect(X, X.RootWin(), "Control-mod1-n", true)
 
 	// Finally, start the main event loop. This will route any appropriate
 	// KeyPressEvents to your callback function.
